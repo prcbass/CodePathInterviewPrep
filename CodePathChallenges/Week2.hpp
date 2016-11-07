97276013d988be88389a710456cce3a8c1c1a976
prcbass <cbasshernandez@gmail.com>
2016-11-06 19:11:07 -0500

    Work done to implement LinkedListNode, not complete, bugs present with current implementation

--- CodePathChallenges/Week2.hpp
+++ CodePathChallenges/Week2.hpp
@@ -10,11 +10,121 @@
 #define Week2_hpp
 
 #include <stdio.h>
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
 class Week2{
-    
+public:
 };
 
+/*
+ NOTE: This implementation of a LinkedList was specified by CodePath and does not represent
+ how I would go about implementing such a data struture. For my implementation go to
+ https://github.com/prcbass/Algs-Datastructs
+*/
+
+template<class T>
+class LinkedListNode{
+public:
+    T data;
+    LinkedListNode<T> *next;
+    LinkedListNode<T>* setValuesFromArray(vector<T> listValues);
+    
+    T getValue();
+    void setValue(T value);
+    
+    LinkedListNode<T> getNext();
+    void setNext(LinkedListNode<T> nextNode);
+    
+    void printList();
+};
+
+template<class T>
+LinkedListNode<T>* LinkedListNode<T>::setValuesFromArray(vector<T> listValues){
+    LinkedListNode<T>* head = this;
+    LinkedListNode<T>* currentNode = this;
+    for(int i=0; i < listValues.size(); i++){
+        currentNode->data = listValues[i];
+        //cout << currentNode->data << ", ";
+        
+        LinkedListNode<T>* nextNode;
+        currentNode->next = nextNode;
+        currentNode = currentNode->next;
+    }
+    //cout << endl;
+    
+//    LinkedListNode<T>* firstNode;
+//    while(firstNode != NULL){
+//        //cout << firstNode->data << ", ";
+//        firstNode = firstNode->next;
+//    }
+    //cout << endl;
+    return head;
+}
+
+template<class T>
+T LinkedListNode<T>::getValue(){
+    return data;
+}
+
+template<class T>
+void LinkedListNode<T>::setValue(T value){
+    data = value;
+}
+
+template<class T>
+LinkedListNode<T> LinkedListNode<T>::getNext(){
+    return next;
+}
+
+template<class T>
+void LinkedListNode<T>::setNext(LinkedListNode<T> nextNode){
+    next = nextNode;
+}
+
+template<class T>
+void LinkedListNode<T>::printList(){
+    LinkedListNode<T> *currentNode = this;
+    while(currentNode != NULL){
+        cout << currentNode->data << " -> ";
+        currentNode = currentNode->next;
+    }
+    cout << endl;
+}
+
 #endif /* Week2_hpp */
+
+
+/*
+ using namespace std;
+ 
+ class LinkedListNode{
+ public:
+ int val;
+ LinkedListNode *next;
+ 
+ LinkedListNode(int node_value) {
+ val = node_value;
+ next = NULL;
+ }
+ };
+ 
+ LinkedListNode* _insert_node_into_singlylinkedlist(LinkedListNode *head, LinkedListNode *tail, int val){
+ if(head == NULL) {
+ head = new LinkedListNode(val);
+ tail = head;
+ }
+ else {
+ LinkedListNode *node = new LinkedListNode(val);
+ tail->next = node;
+ tail = tail->next;
+ }
+ return tail;
+ }
+ 
+ 
+ 
+ */
+
--- CodePathChallenges/main.cpp
+++ CodePathChallenges/main.cpp
@@ -8,9 +8,11 @@
 
 #include <iostream>
 #include "Week1.hpp"
+#include "Week2.hpp"
 using namespace std;
 
 int main(int argc, const char * argv[]) {
+    /*
     Week1 week;
     
     vector<int> arr {2,3,3,3,5,5,7,11,11,13,14,14,14,15,16,17};
@@ -24,6 +26,13 @@ int main(int argc, const char * argv[]) {
     cout << endl;
     
     cout << "Num valid elements: " <<  num << endl;
+     */
+    
+    vector<int> arr {1, 4, 5, 7, 8, 2};
+    LinkedListNode<int>* head;
+    head = head->setValuesFromArray(arr);
+    head->printList();
+    
     
     return 0;
 }
